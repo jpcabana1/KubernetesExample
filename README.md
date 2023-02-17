8e2d40eb2b038c0f5d017f8e404246d4b0d89c37
João Pedro <jpcabana1@gmail.com>
2023-02-17 02:37:25 -0300

    configurações

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # KubernetesExample
+
+## Lista de Comandos
+
+### Instalação
+- function kubectl { minikube kubectl -- $args }
+
+### Pushing images(usando o docker daemon dentro do minikube)
+- & minikube -p minikube docker-env --shell powershell | Invoke-Expression
+
+### Deploy
+- kubectl get deployments
+- kubectl apply --filename deployment.yaml
+- kubectl delete --filename deployment.yaml
+- kubectl logs kubernetes-example-6f854df45b-9drjj
+
+## Outros
+- docker build --tag app1 ./app1
+- docker build --tag app2 ./app2
--- app1/Program.cs
+++ app1/Program.cs
@@ -5,11 +5,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -19,7 +17,7 @@ app.MapGet("/weatherforecast", async (IConfiguration configuration, Cancellation
     HttpClient client = new HttpClient();
     var resp = Enumerable.Empty<WeatherForecast?>();
 
-    HttpResponseMessage response = await client.GetAsync(configuration.GetSection("Urls:app2").Get<string>() + "/weatherforecast");
+    HttpResponseMessage response = await client.GetAsync(configuration.GetSection("Urls:App2").Get<string>() + "/weatherforecast");
     if (response.IsSuccessStatusCode && response!.Content != null)
     {
         resp = await response!.Content!.ReadFromJsonAsync<IEnumerable<WeatherForecast?>>(cancellationToken: cancellationToken)!;
